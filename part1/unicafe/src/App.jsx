1ecbe1d05c2b21b3c0dd0e73320b6d67bb11d0e7
Mladen Savic <mladen.savic@pricespider.com>
2025-07-10 14:28:36 +0200

    finished exercise 1.8, unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import Statistics from "./components/Statistics";
+
 const App = () => {
   // state for feedback counts
   const [good, setGood] = useState(0);
@@ -11,11 +13,6 @@ const App = () => {
   const handleNeutralClick = () => setNeutral(neutral + 1);
   const handleBadClick = () => setBad(bad + 1);
 
-  // additional stats
-  const total = good + neutral + bad;
-  const average = total === 0 ? 0 : (good - bad) / total;
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100;
-
   return (
     <div>
       <h1>unicafe feedback</h1>
@@ -23,13 +20,7 @@ const App = () => {
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
 
-      <h2>stats</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercentage} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
---
+++ part1/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,19 @@
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positive = total === 0 ? 0 : (good / total) * 100;
+
+  return (
+    <div>
+      <h2>stats</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  );
+};
+
+export default Statistics;
